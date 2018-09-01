3c6d4101669085e57e621d0500c7cbd74c6d612d
Krzysztof Błażełek <kblazelek@gmail.com>
2018-09-01 18:06:49 +0200

    Added script that plots ERP

---
+++ EEGAnalysis/EEG_ERPPlot.py
@@ -0,0 +1,27 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import FlightData
+import CustomFigure
+import os
+
+erpDir = './Images/ERP'
+if not os.path.exists(erpDir):
+    os.makedirs(erpDir)
+flightNumber = 3
+removeArtifacts = True
+(trials, times, sample_rate, channels) = FlightData.get_trials(flightNumber, 150, 150, removeArtifacts)
+
+# ERP plot
+for channelIndex in range(0, len(channels)):
+#channelIndex = 12
+    CustomFigure.get_custom_figure()
+    plt.plot(times, trials[:, channelIndex, :], color='k')
+    plt.plot(times, np.mean(trials[:, channelIndex, :], 1), linewidth=2, color='r')
+    plt.xlabel('Czas [ms]')
+    plt.ylabel(r'Amplituda [${\mu V}$]')
+    if removeArtifacts:
+        filename = f"{erpDir}/ERP{flightNumber}_{channels[channelIndex]}_NoArtifacts.png"
+    else:
+        filename = f"{erpDir}/ERP{flightNumber}_{channels[channelIndex]}_WithArtifacts.png"
+    plt.savefig(filename, format='png', bbox_inches='tight')
+#plt.show()
