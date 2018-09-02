eb4b3346e5a88e0b00bfca2defc49da8225bdd58
Krzysztof Błażełek <kblazelek@gmail.com>
2018-09-02 14:45:10 +0200

    Added ability to return reaction times that resulted from artifact-free trials

--- EEGAnalysis/FlightData.py
+++ EEGAnalysis/FlightData.py
@@ -3,34 +3,40 @@ from pandas import read_csv, DataFrame
 import TrialExtractor as te
 import FutureEventResultParser
 
+
+def get_trials_to_skip(flight_number):
+    if flight_number == 1:
+        # 22 and 51 was detected using butterfly plot
+        # 0, 1, 2, 4, 8, 24, 25, 26, 28, 29, 38, 43, 44, 45 was detected using ERP plot
+        return [0, 1, 2, 4, 8, 22, 24, 25, 26, 28, 29, 38, 43, 44, 45, 51]
+    elif flight_number == 2:
+        # 51 was detected using butterfly plot
+        # 13, 14, 25, 36, 37, 38 was detected using ERP plot
+        return [13, 14, 25, 36, 37, 38, 51]
+    elif flight_number == 3:
+        # all were detected by ERP plot
+        return [0, 1, 3, 4, 7, 8, 9, 10, 11, 13, 14, 15, 16, 17, 18, 19, 21, 22, 23, 24, 25, 26, 29, 36, 37,
+                38, 39, 42, 45, 46, 55, 56]
+    else:
+        raise ValueError(f"There was no flight with number {flight_number}")
+
+
 def get_trials(flight_number, samples_before_event, samples_after_event, remove_artifacts=True):
     channels = ['AF3', 'F7', 'F3', 'FC5', 'T7', 'P7', 'O1', 'O2', 'P8', 'T8', 'FC6', 'F4', 'F8', 'AF4']
     channels_to_skip = []
-    trials_to_skip = []
+    trials_to_skip = get_trials_to_skip(flight_number)
     if flight_number == 1:
         data = read_csv('./Data/2018.17.8_14.31.16_EEG_With_Header.csv', sep=";")
         sample_rate = 128.0
         channels_to_skip = ['T8']
-        # 22 and 51 was detected using butterfly plot
-        # 0, 1, 2, 4, 8, 24, 25, 26, 28, 29, 38, 43, 44, 45 was detected using ERP plot
-        trials_to_skip = [0, 1, 2, 4, 8, 22, 24, 25, 26, 28, 29, 38, 43, 44, 45, 51]
     elif flight_number == 2:
         data = read_csv('./Data/2018.08.20_13.59.46_EEG.csv', sep=";")
         sample_rate = 128.0
         channels_to_skip = ['T8']
-        # 51 was detected using butterfly plot
-        # 13, 14, 25, 36, 37, 38 was detected using ERP plot
-        trials_to_skip = [13, 14, 25, 36, 37, 38, 51]
     elif flight_number == 3:
         data = read_csv('./Data/2018.08.30_13.51.46_EEG.csv', sep=";")
         sample_rate = 128.0
         channels_to_skip = ['F4']
-        # all were detected by ERP plot
-        trials_to_skip = [0, 1, 3, 4, 7, 8, 9, 10, 11, 13, 14, 15, 16, 17, 18, 19, 21, 22, 23, 24, 25, 26, 29, 36, 37,
-                          38, 39, 42, 45, 46, 55, 56]
-    elif flight_number == 4:
-        data = read_csv('./Data/Example.csv', sep=";")
-        sample_rate = 256.0
     else:
         raise ValueError(f"There was no flight with number {flight_number}")
 
@@ -58,12 +64,30 @@ def get_trials(flight_number, samples_before_event, samples_after_event, remove_
 
     return trials, times, sample_rate, channels
 
-def get_future_event_set_result(flight_number):
-    if flight_number == 1:
-        reactionTimes, arrows, delays = FutureEventResultParser.parse('./Data/2018.17.8_15.32.00_ReactionTimes.xml')
-    elif flight_number == 2:
-        reactionTimes, arrows, delays = FutureEventResultParser.parse('./Data/2018.08.20_15.00.21_ReactionTimes.xml')
-    elif flight_number == 3:
-        reactionTimes, arrows, delays = FutureEventResultParser.parse('./Data/2018.08.30_14.52.28_ReactionTimes.xml')
 
-    return reactionTimes, arrows, delays
+def get_future_event_set_result(flight_number, remove_data_from_faulty_trials=True):
+    if flight_number == 1:
+        reaction_times, arrows, delays = FutureEventResultParser.parse('./Data/2018.17.8_15.32.00_ReactionTimes.xml')
+    elif flight_number == 2:
+        reaction_times, arrows, delays = FutureEventResultParser.parse('./Data/2018.08.20_15.00.21_ReactionTimes.xml')
+    elif flight_number == 3:
+        reaction_times, arrows, delays = FutureEventResultParser.parse('./Data/2018.08.30_14.52.28_ReactionTimes.xml')
+    else:
+        raise ValueError(f"There was no flight with number {flight_number}")
+
+    if remove_data_from_faulty_trials:
+        # Get indexes of trials containing artifacts
+        trials_to_skip = get_trials_to_skip(flight_number)
+
+        # Get all indexes in array
+        indexes_to_keep = np.arange(len(reaction_times))
+
+        # Remove indexes that we want to skip
+        indexes_to_keep = [i for i in indexes_to_keep if i not in trials_to_skip]
+
+        # Filter arrays so that only data for artifact-free trials remain
+        reaction_times = reaction_times[indexes_to_keep]
+        arrows = arrows[indexes_to_keep]
+        delays = delays[indexes_to_keep]
+
+    return reaction_times, arrows, delays
